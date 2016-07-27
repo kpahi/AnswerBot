f5199b7cc4bb0777d315c04ca4a30c13627fe855
Basanta78 <basmhzn78@gmail.com>
2016-07-27 15:52:32 +0545

    Generate list of senteces

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import sys
 import random
+import nltk
 sInput = " "
 sRespons = " "
 
 from tokeninput import *
 from wikisearch import *
 from scraping import *
+from response import resfunctions
 
+res = resfunctions()
 while 1:
-	print("> ", end = '')
-	sInput = input()
-	alltokens = tokenization(sInput)
-	print(alltokens)
-	nouns = grouping(alltokens)
-	print(nouns)
+    print("> ", end = '')
+    sInput = input()
+    '''alltokens = tokenization(sInput)
+    print(alltokens)
+    nouns = grouping(alltokens)
+    print(nouns)'''
 
+    tokens = res.word_tokenize(sInput)
+    nostop_words = res.remove_stopwords(tokens)
 #get the wiki link for corresponding nouns
-	link = getlink(nouns)
-	
-	
-	content = getdes(link)
-	nouns.clear()
+    link = getlink(nostop_words)
 
+
+    content = regetdes(link)
+    print(content)
+    '''file = open("paragraph.txt",'r')
+    readfile =file.read()
+    print(readfile)
+    sentences = nltk.sent_tokenize(readfile)
+    print(sentences)'''
 
